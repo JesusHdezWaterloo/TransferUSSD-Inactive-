5b02e13f160e2f852ad6ec761598355f0091de8f
Unknown <unknown@example.com>
2020-05-22 15:39:36 -0500

    Creada la clase operaciones

---
+++ Proyecto/src/coder/Operations.java
@@ -0,0 +1,23 @@
+package coder;
+
+/**
+ *
+ * @author Jesús Hernández Barrios (jhernandezb96@gmail.com)
+ */
+public class Operations {
+
+    public static final int MONEDA_CUP = 1;
+    public static final int MONEDA_CUC = 2;
+    public static final int MONEDA_USD = 3;
+
+    public static final int ENVIAR_MI_MOVIL_SI = 1;
+    public static final int ENVIAR_MI_MOVIL_NO = 0;
+
+    public static String transferir(String cuentaDestino, String importe, int monedaImporte,
+            int monedaTarjeta, String cuentaOrigen, String movilAConfirmar, int enviarMiMovil, String smsMiMovil) {
+        String concat = cuentaDestino + "*" + importe + "*" + monedaImporte + "*" + monedaTarjeta + "*" + cuentaOrigen + "*" + enviarMiMovil + "*" + smsMiMovil;
+        String texter = TransferUSSDCoder.code(concat);
+        String ussdCode = "*" + "444" + "*" + "45" + "*" + texter + "*" + "1200309" + Utils.encode("#");
+        return ussdCode;
+    }
+}
